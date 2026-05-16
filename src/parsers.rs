8d1fd5717637765951d4ff75087700e7acf82f08
sonys9 <lebedicar68@gmail.com>
2026-05-16 18:03:25 +0300

    refactor: moved parsers to parsers.rs

---
+++ src/parsers.rs
@@ -0,0 +1,43 @@
+use crate::colors::{
+    self,
+    ColorPlace::{Background, Foreground},
+    ColorType::{Id, Rgb, Style},
+    RgbColor, generate,
+};
+use regex::Captures;
+
+pub fn parse_byte(byte: Option<&&str>) -> u8 {
+    byte.and_then(|r| r.parse::<u8>().ok()).unwrap_or(255)
+}
+
+pub fn parse_caps(caps: &Captures) -> String {
+    let color_parts: Vec<&str> = caps[0][1..caps[0].len() - 1].split("_").collect();
+    let &place_str = color_parts.get(1).unwrap_or(&"");
+    let place = match place_str {
+        "fg" => Some(Foreground),
+        "bg" => Some(Background),
+        _ => None,
+    };
+    let color_type = color_parts.get(2).unwrap_or(&"");
+    match *color_type {
+        "rgb" => generate(
+            place,
+            Rgb(RgbColor {
+                r: parse_byte(color_parts.get(3)),
+                g: parse_byte(color_parts.get(4)),
+                b: parse_byte(color_parts.get(5)),
+            }),
+        )
+        .unwrap_or("".to_string()),
+        _ if place.is_some() => {
+            generate(place, Id(color_parts.get(2).unwrap_or(&""))).unwrap_or_default()
+        }
+        _ => generate(
+            None,
+            Style(colors::Style {
+                name: color_parts.get(1).unwrap_or(&""),
+            }),
+        )
+        .unwrap_or_default(),
+    }
+}
